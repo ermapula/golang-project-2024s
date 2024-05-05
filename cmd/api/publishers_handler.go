cf72a2e057516056e4fe53af339a7a2e0262d982
Ermek <tolymbekov_ermek@mail.ru>
2024-05-05 15:36:37 +0600

    fix: cleanup

--- cmd/api/publishers_handler.go
+++ cmd/api/publishers_handler.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"time"
 )
 
 func (app *application) getPublishers(w http.ResponseWriter, r *http.Request) {
@@ -32,31 +30,4 @@ func (app *application) getPublisher(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app.writeJSON(w, http.StatusOK, envelope{"publisher": publisher}, nil)
-}
-
-func (app *application) convert(w http.ResponseWriter, r *http.Request) {
-	var input struct {
-		Title       string    `json:"title"`
-		Genres      []string  `json:"genres"`
-		ReleaseDate time.Time `json:"releaseDate"`
-		Price       float64   `json:"price"`
-		PublisherId int       `json:"publisherId"`
-	}
-	err := app.readJSON(w, r, &input)
-	if err != nil {
-		app.badRequestResponse(w, r, err)
-		return
-	}
-	s := "INSERT INTO games (title, genres, release_date, price, publisher_id) VALUES ("
-	s += fmt.Sprintf(`'%v', '{`, input.Title)
-	for i, a := range input.Genres {
-		if i != len(input.Genres) - 1 {
-			s += fmt.Sprintf(`"%v", `, a)
-		} else {
-			s += fmt.Sprintf(`"%v"}', `, a)
-		}
-	}
-	s += fmt.Sprintf(`'%v', %v, %v);`, input.ReleaseDate.Format("2006-01-02"), input.Price, input.PublisherId)
-
-	fmt.Fprintf(w, s)
 }
